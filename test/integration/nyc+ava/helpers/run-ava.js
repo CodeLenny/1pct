dc3380dec708d99747672d814b137e145f0b20b8
Ryan Leonard <rhinoisnot@gmail.com>
2017-07-23 13:56:43 -0400

    Fixed testing to use a new temporary directory for each run.

--- test/integration/nyc+ava/helpers/run-ava.js
+++ test/integration/nyc+ava/helpers/run-ava.js
@@ -1,5 +1,6 @@
 const fs = require("fs-extra");
 const path = require("path");
+const tmp = require("tmp-promise");
 const { exec } = require("child-process-promise");
 const either = require("ethr");
 
@@ -7,23 +8,22 @@ const pct = require.resolve("1pct");
 const nyc = "node_modules/.bin/nyc";
 const ava = "node_modules/.bin/ava";
 
-function run(cwd) {
+function run(src) {
   const salt = Math.random().toString(36).slice(2);
-  const nycOutput = path.join(cwd, `.nyc_output_${salt}`);
-  let testExec, output;
-  return Promise
-    .all([
+  let cwd, testExec, output;
+  return tmp
+    .tmpName()
+    .then(d => cwd = d)
+    .then(() => fs.copy(src, cwd))
+    .then(() => Promise.all([
       fs.ensureSymlink(path.join(process.env.NODE_PATH, "node_modules"), path.join(cwd, "node_modules")),
       fs.ensureSymlink(path.join(process.env.NODE_PATH, "support/package.json"), path.join(cwd, "package.json")),
-      fs.ensureDir(nycOutput),
       fs.ensureDir(path.join(cwd, "coverage")),
-    ])
+    ]))
     .then(() => {
-      const tmpFlag = `--temp-directory="${nycOutput}"`;
       const report = "--reporter=text --reporter=json-summary";
-      //const exclude = `-x='["**/test/*.js"]'`;
       const files = `${cwd}/test/**`;
-      testExec = `node ${nyc} ${report} ${tmpFlag} ${ava} -v ${files}`;
+      testExec = `node ${nyc} ${report} ${ava} -v ${files}`;
       return exec(testExec, { cwd });
     })
     .then(() => exec(`node ${pct}`, { cwd }))
@@ -36,7 +36,7 @@ function run(cwd) {
         counted,
       });
     })
-    .then(...either(() => fs.remove(nycOutput)))
+    .then(...either(() => fs.remove(cwd)))
     .then(() => output);
 }
 
